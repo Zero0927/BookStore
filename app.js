2629a34f82989228379da85ed02b552a3f6cbafe
Scott Zhou <zhouscott@hotmail.com>
2021-08-02 15:37:27 -0700

    Import back end code

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+
+var compression = require('compression');
+var helmet = require('helmet');
+
+var app = express();
 
 
-//app.set('view engine', 'ejs');
-//app.set('views', path.join(__dirname, '/views'))
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(express.static(path.join(__dirname, '/dist')))
 
-var MongoClient = require('mongodb').MongoClient;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/BookstoreDB", function (err, db) {  
-     if(err) throw err;
-     //Write databse Insert/Update/Query code here..
-      console.log("Database has been created")          
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(compression()); // Compress all routes
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/', (req,res)=>{
-  res.render('home.ejs')
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-app.listen(port, () => {
-  console.log(`Listening at port:${port}`)
-})
+module.exports = app;
